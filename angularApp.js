a4b9689b92d386cc882e311c0d0abb8823490cec
Mohammad Nurul Amin <mnurulamin092@outlook.com>
2018-08-08 08:47:14 +0600

    cost calculator

--- angularApp.js
+++ angularApp.js
@@ -1,5 +1,13 @@
-var app = angular.module("app",[]);
+/*var app = angular.module("app",[]);
      app.controller('myCtrl', function($scope){
     $scope.firstName = "Rushda";
     $scope.lastName = "Saba";
+     });
+     */
+
+     var app = angular.module("myApp",[]);
+     app.directive("w3TestDirective", function(){
+        return{
+            template: " I was made directiove contorlller"
+};
      });
